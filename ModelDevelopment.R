e17039059ed5363837fbe17bcf419b6c08da1d57
Jesse Bishop (WHRC) <jbishop@whrc.org>
2015-06-12 20:46:01 -0400

    renaming

--- ModelDevelopment.R
+++ ModelDevelopment.R
@@ -1,5 +1,6 @@
 library(RPostgreSQL)
 library(randomForest)
+library(sampling)
 con <- dbConnect(drv="PostgreSQL", host="pan.whrc.ad", user="jbishop", dbname="smddb")
 query <- "SELECT z.folio, z.carbono_arboles_tpha, z.date_distance, z.measurement_date, o.id_vegetac AS veg_type_id, o.vegetacion AS veg_type, CASE WHEN o.vegetacion IN ('Bosque de abies', 'Bosque de ayarin', 'Bosque de cedro', 'Bosque de pino', 'Bosque de tascate') THEN 'CF' WHEN o.vegetacion IN ('Bosque de encino', 'Bosque de galerÝa') THEN 'BF' WHEN o.vegetacion IN ('Bosque de encino-pino', 'Bosque de pino-encino') THEN 'CBF' WHEN o.vegetacion = 'Manglar' THEN 'MG' WHEN o.vegetacion IN ('Bosque mesofilo de monta±a', 'Selva alta perennifolia', 'Selva alta subperennifolia', 'Selva baja perennifolia', 'Selva baja subperennifolia', 'Selva mediana subperennifolia') THEN 'THF' WHEN o.vegetacion IN ('Selva baja caducifolia', 'Selva baja espinosa', 'Selva baja subcaducifolia', 'Selva mediana caducifolia', 'Selva mediana subcaducifolia') THEN 'TDF' ELSE 'ERROR' END AS type_code, o.ecosistema AS ecosystem, AVG(s.num_pixels) AS num_pixels, AVG(s.num_masked_pixels) AS num_masked_pixels, AVG(s.elev_mean) AS elev_mean, AVG(s.slope_mean) AS slope_mean, AVG(s.vcf_mean) AS vcf_mean, AVG(hh_mean) AS hh_mean, AVG(hv_mean) AS hv_mean, AVG(lsmask_mean) AS lsmask_mean, MIN(lsmask_min) AS lsmask_min, MAX(lsmask_max) AS lsmask_max, COUNT(alos_id) AS num_images FROM (SELECT b.folio, SUM(b.carbono_arboles) / 0.1598925 AS carbono_arboles_tpha, y.date_distance, MIN(b.levantamiento_fecha_ejecucion) AS measurement_date  FROM (SELECT folio, CASE WHEN MIN(date_distance) + MAX(date_distance) = 0 THEN MIN(date_distance) WHEN MIN(date_distance) + MAX(date_distance) > 0 THEN MIN(date_distance) ELSE MAX(date_distance) END AS date_distance FROM (SELECT folio, days_from_alos AS date_distance, COUNT(sitio) AS plot_count FROM mexico_biomass_plots_filtered WHERE NOT carbono_arboles IS NULL AND NOT levantamiento_fecha_ejecucion IS NULL AND folio IN (SELECT folio FROM mexico_biomass_plots_old) AND NOT tipificacion IN ('Inaccesible (pendiente)', 'Inaccesible (social)', 'Vacio', 'Planeado') GROUP BY folio, days_from_alos HAVING COUNT(sitio) = 4 ORDER BY folio) AS x GROUP BY folio) AS y INNER JOIN mexico_biomass_plots_filtered b ON b.folio=y.folio AND b.days_from_alos=y.date_distance GROUP BY b.folio, y.date_distance) AS z INNER JOIN mexico_biomass_plots_old o ON z.folio=o.folio INNER JOIN mexico_biomass_plots_model_statistics s ON z.folio=s.folio GROUP BY z.folio, z.carbono_arboles_tpha, z.date_distance, z.measurement_date, o.id_vegetac, o.vegetacion, o.ecosistema;"
 d <- dbGetQuery(con, query)
@@ -48,27 +49,82 @@ sub <- subset(sub, ! folio %in% f)
 plot(sub$carbono_arboles_tpha, sub$hv_mean)
 plot(sub$carbono_arboles_tpha, sub$vcf_mean)
 
-# Split out testing and training
+# Split out testing and training - TC START
+# Define carbon classes in 10 t/ha increments (rounding up to nearest 10)
+roundUp <- function(x,to=10) to*(x%/%to + as.logical(x%%to))
+maxC <- roundUp(max(sub$carbono_arboles_tpha))
+c.classes <- seq(from=0, to=maxC, by=10)
 
-attach(sub)
-#stack <- data.frame(hh_mean, hv_mean, vcf_mean, slope_mean, elev_mean)
+# For each class, assign field plots based on C value
+for (class in 1:(length(c.classes)-1)) {
+  sub$claseCarbona[sub$carbono_arboles_tpha >= c.classes[class] & sub$carbono_arboles_tpha < c.classes[class+1]] <- class*10
+}
+
+# Now combine type_code and claseCarbona into one field for ease of stratification (is this ok to do??)
+sub$grp <- paste(sub$type_code, sub$claseCarbona, sep="_")
+
+stratified = function(df, group, size) {
+  #  USE: * Specify your data frame and grouping variable (as column 
+  #         name) as the first two arguments.
+  #       * Decide on your sample size. For a sample proportional to the
+  #         population, enter "size" as a decimal. For an equal number 
+  #         of samples from each group, enter "size" as a whole number.
+  #
+  #  Example 1: Sample 10% of each group from a data frame named "z",
+  #             where the grouping variable column name is "nameGrpCol", use:
+  # 
+  #                 > stratified(z, "nameGrpCol", .1)
+  #
+  #  Example 2: Sample 5 observations from each group from a data frame
+  #             named "z"; grouping variable column name is "nameGrpCol", use:
+  #
+  #                 > stratified(z, "nameGrpCol", 5)
+  #
+  require(sampling)
+  
+  # column # for group
+  group <- which(names(df) %in% group, arr.ind = T)
+  temp = df[order(df[group]),]
+  # Calc # of samples per group
+  if (size < 1) {
+    nsamp = ceiling(table(temp[group]) * size)
+  } else if (size >= 1) {
+    nsamp = rep(size, times=length(table(temp[group])))
+  }  
+  # Select sample
+  strat = strata(temp, stratanames = names(temp[group]), 
+                 size = as.vector(nsamp), method = "srswor")
+  (dsample = getdata(temp, strat))
+}
+
+# Order sub first, even though it's done in the stratified function
+set.seed(5)
+#sub.ord <- sub[order(sub$grp),]
+sub.train <- stratified(sub, "grp", 0.8)
+sub.val <- sub[(row.names(sub) %in% row.names(sub.train))==F,]
+
+attach(sub.val)
+val.stk <- data.frame(hh_mean, hv_mean, vcf_mean, elev_mean)
+detach(sub.val)
+
+attach(sub.train)
 stack <- data.frame(hh_mean, hv_mean, vcf_mean, elev_mean)
-rf <- randomForest(carbono_arboles_tpha ~ ., data=stack, ntree=200)
-detach()
+rf <- randomForest(carbono_arboles_tpha ~ ., data=stack, ntree=200, xtest=val.stk, ytest=sub.val$carbono_arboles_tpha, importance = T)
+detach(sub.train)
 
-save('rf', file='/mnt/t/testing/mexico/biomass/alos/biomass_modeling/workshop_data/workshop_model.RData')
+save('rf', file='workshop_model.RData')
 
-varImpPlot(rf)
+varImpPlot(rf, type=1)
 
+# Some model evaluation
+n.mod <- length(sub.train$carbono_arboles_tpha)
+r2.mod <- round(1 - sum((sub.train$carbono_arboles_tpha - rf$predicted)^2)/sum((sub.train$carbono_arboles_tpha - mean(sub.train$carbono_arboles_tpha))^2), digits=2)
+adj.r2.mod <- round(1 - (1 - r2.mod)*(n.mod - 1)/ (n.mod - ncol(stack) - 1), digits=2)
+rmse.mod <- round(sqrt(sum((sub.train$carbono_arboles_tpha-rf$predicted)^2)/n.mod), digits=2)
 
+# Same stats for validation data set
+n.val <- length(sub.val$carbono_arboles_tpha)
+r2.val <- round(1 - sum((sub.val$carbono_arboles_tpha - rf$test$predicted)^2)/sum((sub.val$carbono_arboles_tpha - mean(sub.val$carbono_arboles_tpha))^2), digits=2)
+adj.r2.val <- round(1 - (1 - r2.val)*(n.val - 1)/ (n.val - ncol(val.stk) - 1), digits=2)
+rmse.val <- round(sqrt(sum((sub.val$carbono_arboles_tpha-rf$test$predicted)^2)/n.val), digits=2)
 
-# Replace this wrong crap below with proper non-crap
-
-sub$pbio <- rf$pred
-sub$pbiomod <- predict(rf, stack)
-cor.mod <- cor(sub$carbono_arboles_tpha,predict(rf,stack))
-cor.test <- cor(sub$carbono_arboles_tpha,rf$pred)
-rmse.mod <- sqrt(sum((sub$carbono_arboles_tpha-predict(rf,stack))^2)/length(sub$carbono_arboles_tpha))
-rmse.test <- sqrt(sum((sub$carbono_arboles_tpha-rf$pred)^2)/length(sub$carbono_arboles_tpha))
-oob.h.cor <- cor.test
-oob.h.rmse <- rmse.test
